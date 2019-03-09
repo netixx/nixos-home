3bba3290c0853a74f8440575df5ca33429e82c75
Netix (Espinet François) <dev.espinetfrancois@gmail.com>
2019-03-09 09:42:58 +0100

    Fixing netdata GUI configuration

--- monitoring.nix
+++ monitoring.nix
@@ -4,6 +4,9 @@
   services.netdata = {
       enable= true;
       configText = ''
+        [global]
+          web files owner = root
+          web files group = root
         [registry]
           enabled = ${if config.home.netdataRegistry then "yes" else "no"}
           registry to announce = http://10.0.254.254:19999
